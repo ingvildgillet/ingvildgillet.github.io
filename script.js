1f15b52221014c197b8f43865d2d2ad062668515
ingvildgillet <ingvild.gillet@gmail.com>
2023-03-28 13:18:41 +0200

    new website

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const contactButton = document.getElementById("contactButton");
+    
+    contactButton.addEventListener("click", function() {
+        alert("Thank you for clicking the button!");
+    });
+});
